02723733ddb911f34dc88d92b2c0d8c43264dabf
Eduard Bardají Puig <ebardaji@gmail.com>
2016-10-14 23:45:57 +0200

    completes React challenge

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Imersivo coding challenge
 Coding challenge for Full Stack Engineer position.
+
+## React
+To run the React challenge,
+
+```sh
+npm install
+npm run build # runs babel
+npm run live  # runs localhost:3000
+```
+
+There are four components:
+* App - Enclosing component.
+* Square - Displays an array of items arranged into a square shape.
+* Diamond - Displays an array of items arranged into a diamond shape.
+* Subheading - Displays active shape.
+
+### App
+
+The app component houses the other three components as well as two buttons. Clicking on a button changes the state of App. This state is then used in the render function to determine which shape component should be rendered.
+
+### Square
+
+The Square component draws a square-like figure by laying out rows (`<div class=row>`) of items (`<div class=item>`) which are currently red circles. The items have assigned an animation to fade them in (`@keyframes fadeIn`) upon being attached to the DOM. To simulate that a square is progressively drawn, within its render method, each item is given an inline `animation-delay` based on its position.
+
+The value of the last delay is then used to set a timeout for changing the subheading text. An extra second, 1000ms, is added to the delay since the animation to fade in an items lasts an additional second after the delay. The subheading's `setState()` function has been made available globally to allow for easy state setting (more on this in the Subheading's setion).
+
+An IIFE is used to construct an array of rows, which in turn uses another IIFE to create an array of items. These arrays, being component arrays, render their elements in order.
+
+The size of the square may be chosen by using a `size` attribute. The default size is 5. There is currently no input verification.
+
+### Diamond
+
+Works the same as `Square`, with different row-construction logic to produce the diamond shape.
+
+The size of the diamond is the number of items in its middle row.
+
+### Subheading
+
+Displays the subheading, which is either `Current Shapes` or the shape being shown. Upon being mounted, its `setState()` function is made available in a global object, `gsub`. This global object is intended to store the name of the last shape the user requested be drawn as well as an id (in the form of a timestamp). This id is used in the function that is timed out within the shapes' render methods. The effect this has is to check whether the most recent shape requested (the most recent button clicked) is in fact the one that generated the very same timed-out function that is running. This is because the user could request a different shape before the current one is actually fully drawn on screen.
---
+++ app.jsx
@@ -0,0 +1,141 @@
+var gsub = {};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shape: 'Current Shapes'
+    }
+  }
+  render() {
+    return (
+      <div> 
+        <header>
+          <h1>Geometric shapes</h1>
+          <Subheading />
+        </header>
+        <div className="buttons">
+          <div
+            role="button"
+            className="square-button"
+            onClick={() => (this.setState({shape: 'Square'}))}
+            style={{fontWeight: this.state.shape === 'Square' ? 'bold' : null}}
+          >Square</div>
+          <div
+            role="button"
+            className="diamond-button"
+            onClick={() => (this.setState({shape: 'Diamond'}))}
+            style={{fontWeight: this.state.shape === 'Diamond' ? 'bold' : null}}
+          >Diamond</div>
+        </div>
+        <div className="shapes">
+          {(() => {
+            if (this.state.shape === 'Square') return <Square />;
+            else if (this.state.shape === 'Diamond') return <Diamond />;
+          })()}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Diamond extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: props.size || 5
+    }
+  }
+  render() {
+    return (
+      <div className="square">
+        {(() => {
+          var rows = [];
+          var delay;
+          for (var row = 0; row < 2 * this.state.size; row++) {
+            rows.push(<div className="row" key={'r' + row}>
+              {(() => {
+                var items = [];
+                var limit = row > this.state.size ? 2 * this.state.size - row : row;
+                for (var item = 0; item < limit; item++) {
+                  items.push(<div style={{
+                    animationDelay: (delay = (row * 0.1 + item * 0.1)) + 's'
+                  }} className="item" key={item}></div>);
+                }
+                return items;
+              })()}
+            </div>);
+          }
+          var id = gsub.id = Date.now();
+          gsub.text = 'Diamond';
+          setTimeout(() => {
+            if (gsub.id === id) {
+              gsub.setState({text: gsub.text});
+            }
+          }, delay * 1000 + 1000);
+          return rows;
+        })()}
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: props.size || 5
+    }
+  }
+  render() {
+    return (
+      <div className="square">
+        {(() => {
+          var rows = [];
+          var delay;
+          for (var row = 0; row < this.state.size; row++) {
+            rows.push(<div className="row" key={'r' + row}>
+              {(() => {
+                var items = [];
+                for (var item = 0; item < this.state.size; item++) {
+                  items.push(<div style={{
+                    animationDelay: (delay = (row * 0.1 + item * 0.1)) + 's'
+                  }} className="item" key={item}></div>);
+                }
+                return items;
+              })()}
+            </div>);
+          }
+          var id = gsub.id = Date.now();
+          gsub.text = 'Square';
+          setTimeout(() => {
+            if (gsub.id === id) {
+              gsub.setState({text: gsub.text});
+            }
+          }, delay * 1000 + 1000);
+          return rows;
+        })()}
+      </div>
+    );
+  }
+}
+
+class Subheading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+  componentDidMount() {
+    gsub.setState = (s) => {
+      this.setState(s);
+    }
+  }
+  render() {
+    var text = this.state.text || 'Current Shapes';
+    return <h2 className={this.state.text ? 'white' : null}>{text}</h2>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
